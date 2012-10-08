6dabeb3f8927769bb2f16ad1b30b6b02b674ce61
Ryan Griffith <ryan.griffith@hannonhill.com>
2012-10-08 16:09:57 -0400

    Added string encoding helper.

---
+++ config/initializers/stringencoding.rb
@@ -0,0 +1,26 @@
+require 'zlib'
+require 'digest/md5'
+require 'base64'
+
+class String
+	def str_to_crc32
+		Zlib.crc32(self)
+	end
+
+	def str_to_md5
+		Digest::MD5.hexdigest(self)
+	end
+
+	def base64_encode
+		Base64.strict_encode64(self)
+	end
+
+	def base64_decode
+		Base64.decode64(self)
+	end
+
+	def json_escape
+		result = self.to_s.gsub('/', '\/')
+		self.html_safe? ? result.html_safe : result
+	end
+end
