84809eaeca8cce8e93f340dbe8acf925cc1c83eb
Ryan Griffith <ryan.griffith@hannonhill.com>
2012-10-15 10:00:37 -0400

    Removed debugging

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,8 +23,6 @@ class SessionsController < ApplicationController
 	def create
 		auth = request.env["omniauth.auth"]
 
-		logger.debug(auth)
-
 		if auth[:uid]
 			user = User.where(:provider => auth[:provider], 
 			            :uid => auth[:uid]).first || User.create_with_omniauth(auth)
